4d4dc019d84727289172d0c7f1f9ea2bf5d2bc9d
anani <abood.anani57@gmail.com>
2022-10-13 15:45:33 +0300

    idk

--- store/settings.py
+++ store/settings.py
@@ -113,7 +113,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ROOT_URLCONF = 'store.urls'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 CART_SESSION_ID = 'cart'
